a7d53d33dcb7de10d278b614c9eaa753c0648018
Дмитро <028dima@gmail.com>
2023-06-17 12:02:28 +0300

    23432

---
+++ src/HW1/Task1.java
@@ -0,0 +1,25 @@
+package HW1;
+
+import java.util.*;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("введіть декілька цифр для реверсу");
+        Scanner scanner = new Scanner(System.in);
+        int intscan = scanner.nextInt();
+        reversN(intscan);
+        }
+           public static void reversN(int N){
+        if (N<10){
+            System.out.print(N);
+            } else {
+            System.out.print(N%10 + ",");
+           reversN(N/10);
+        }
+             }
+
+
+
+
+
+}
