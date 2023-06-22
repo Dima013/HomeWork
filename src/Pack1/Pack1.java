6a61f5d4b970bfd9c290c3c12535ba7e0abf7525
Dima013 <028dima@gmail.com>
2023-06-22 22:23:12 +0300

    add new symbol

---
+++ src/Pack1/Pack1.java
@@ -0,0 +1,7 @@
+package Pack1;
+
+public class Pack1 {
+    public static void main(String[] args) {
+        System.out.println("jggf");
+    }
+}
---
+++ src/Pack1/Pack2.java
@@ -0,0 +1,7 @@
+package Pack1;
+
+public class Pack2 {
+    public static void main(String[] args) {
+        System.out.println("asdsa");
+    }
+}
