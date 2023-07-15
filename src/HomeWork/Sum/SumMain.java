c8453e2e31df89840171ecc2589ae55764580811
Dima013 <028dima@gmail.com>
2023-07-15 05:43:03 +0300

    changed name of variable add 1 space

--- src/HomeWork/Sum/SumMain.java
+++ src/HomeWork/Sum/SumMain.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 
 public class SumMain {
     public static void main(String[] args) {
-        boolean miss = false;
-       do {
-           try {
-               System.out.println("Enter the required size of the array : ");
-               Scanner s = new Scanner(System.in);
-               int size = s.nextInt();
-               int myArray[] = new int[size];
-               int sum = 0;
-               System.out.println("Enter the elements of the array one by one ");
+        boolean isExit;
+        do {
+            try {
+                System.out.println("Enter the required size of the array : ");
+                Scanner s = new Scanner(System.in);
+                int size = s.nextInt();
+                int myArray[] = new int[size];
+                int sum = 0;
+                System.out.println("Enter the elements of the array one by one ");
 
-               for (int i = 0; i < size; i++) {
-                   myArray[i] = s.nextInt();
-                   sum = sum + myArray[i];
-               }
-               System.out.println("Elements of the array are: " + Arrays.toString(myArray));
-               System.out.println("Sum of the elements of the array :" + sum);
+                for (int i = 0; i < size; i++) {
+                    myArray[i] = s.nextInt();
+                    sum = sum + myArray[i];
+                }
+                System.out.println("Elements of the array are: " + Arrays.toString(myArray));
+                System.out.println("Sum of the elements of the array :" + sum);
 
-           } catch (Exception mistake) {
+            } catch (Exception mistake) {
 
-               System.out.println("Wrong type" + mistake.getMessage());
-               System.out.println("Please try again");
-           }
-       }while (miss = true);
+                System.out.println("Wrong type" + mistake.getMessage());
+                System.out.println("Please try again");
+
+            }
+        } while (isExit = true);
     }
 }
 
