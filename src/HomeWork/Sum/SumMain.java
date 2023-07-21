e7a9f9fa60c8d4cbc8950bef96811af8c4472d7a
Dima013 <028dima@gmail.com>
2023-07-21 23:31:01 +0300

    change

--- src/HomeWork/Sum/SumMain.java
+++ src/HomeWork/Sum/SumMain.java
@@ -11,20 +11,20 @@ public class SumMain {
                 System.out.println("Enter the required size of the array : ");
                 Scanner s = new Scanner(System.in);
                 int size = s.nextInt();
-                int myArray[] = new int[size];
+                int numberFromCosole[] = new int[size];
                 int sum = 0;
                 System.out.println("Enter the elements of the array one by one ");
 
                 for (int i = 0; i < size; i++) {
-                    myArray[i] = s.nextInt();
-                    sum = sum + myArray[i];
+                    numberFromCosole[i] = s.nextInt();
+                    sum += numberFromCosole[i];
                 }
-                System.out.println("Elements of the array are: " + Arrays.toString(myArray));
+                System.out.println("Elements of the array are: " + Arrays.toString(numberFromCosole));
                 System.out.println("Sum of the elements of the array :" + sum);
 
-            } catch (Exception mistake) {
+            } catch (Exception instead) {
 
-                System.out.println("Wrong type" + mistake.getMessage());
+                System.out.println("Wrong type" + instead.getMessage());
                 System.out.println("Please try again");
 
             }
