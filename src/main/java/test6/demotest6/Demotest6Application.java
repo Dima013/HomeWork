3aa5e2389e588f6d4903144260aff8362cf9f42a
Dima013 <028dima@gmail.com>
2023-08-13 22:42:52 +0300

    init commit

---
+++ src/main/java/test6/demotest6/Demotest6Application.java
@@ -0,0 +1,13 @@
+package test6.demotest6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotest6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demotest6Application.class, args);
+	}
+
+}
